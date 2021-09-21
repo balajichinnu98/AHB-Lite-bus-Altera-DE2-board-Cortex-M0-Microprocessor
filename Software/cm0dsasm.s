4a3e6e23b3e838a17b77cee89912ebd73b5be4e8
sgbchan2 <86721104+sgbchan2@users.noreply.github.com>
2021-09-21 13:30:17 +0100

    Add files via upload

---
+++ Software/cm0dsasm.s
@@ -0,0 +1,73 @@
+; Vector Table Mapped to Address 0 at Reset
+
+						PRESERVE8
+                		THUMB
+
+        				AREA	RESET, DATA, READONLY	  			; First 32 WORDS is VECTOR TABLE
+        				EXPORT 	__Vectors
+					
+__Vectors		    	DCD		0x000003FC							; 1K Internal Memory
+        				DCD		Reset_Handler
+        				DCD		0  			
+        				DCD		0
+        				DCD		0
+        				DCD		0
+        				DCD		0
+        				DCD		0
+        				DCD		0
+        				DCD		0
+        				DCD		0
+        				DCD 	0
+        				DCD		0
+        				DCD		0
+        				DCD 	0
+        				DCD		0
+        				
+        				; External Interrupts
+						        				
+        				DCD		0
+        				DCD		0
+        				DCD		0
+        				DCD		0
+        				DCD		0
+        				DCD		0
+        				DCD		0
+        				DCD		0
+        				DCD		0
+        				DCD		0
+        				DCD		0
+        				DCD		0
+        				DCD		0
+        				DCD		0
+        				DCD		0
+        				DCD		0
+              
+                AREA |.text|, CODE, READONLY
+;Reset Handler
+Reset_Handler   PROC
+                GLOBAL Reset_Handler
+                ENTRY
+    
+AGAIN		   	LDR 	R1, =0x52000000
+				LDR		R0, =0x20152794
+				STR		R0, [R1]
+
+				LDR		R0, =0x01
+Loop			SUBS	R0,R0,#1
+				BNE Loop
+
+				LDR 	R1, =0x52000000
+				LDR		R0, =0x94972510
+				STR		R0, [R1]
+
+				LDR		R0, =0x02
+Loop1			SUBS	R0,R0,#1
+				BNE Loop1
+
+				B AGAIN
+				ENDP
+
+
+
+		END                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
+   
